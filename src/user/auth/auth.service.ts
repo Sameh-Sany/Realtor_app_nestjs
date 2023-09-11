232df58290c6ed115ab4ac25dbbe5fa8b9ae1220
Dev-Sameh <92803501+Sameh-Sany@users.noreply.github.com>
2023-09-11 14:58:59 +0300

    edit prisma schecma and add user auth

---
+++ src/user/auth/auth.service.ts
@@ -0,0 +1,81 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+import { UserType } from '@prisma/client';
+
+interface SignupParams {
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+}
+
+interface SigninParams {
+  email: string;
+  password: string;
+}
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async signup({ email, password, name, phone }: SignupParams) {
+    const userExists = await this.prismaService.user.findFirst({
+      where: { email },
+    });
+
+    if (userExists) {
+      throw new BadRequestException();
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password: hashPassword,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    return this.generateJWT(name, user.id);
+  }
+
+  async signin({ email, password }: SigninParams) {
+    const user = await this.prismaService.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new BadRequestException();
+    }
+
+    const hashPassword = user.password;
+
+    const isValidPassword = await bcrypt.compare(password, hashPassword);
+
+    if (!isValidPassword) {
+      throw new BadRequestException();
+    }
+
+    return this.generateJWT(user.name, user.id);
+  }
+
+  private generateJWT(name: string, id: number) {
+    return jwt.sign(
+      {
+        name,
+        id,
+      },
+      process.env.JSON_TOKEN_KEY,
+      {
+        expiresIn: '7d',
+      },
+    );
+  }
+}
---
+++ src/user/dtos/auth.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  MinLength,
+  Matches,
+} from 'class-validator';
+
+export class SignupDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @Matches(/^01[0125][0-9]{8}$/gm, {
+    message: 'phone must be valid phone number',
+  })
+  phone: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(5)
+  password: string;
+}
+
+export class SigninDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+}
